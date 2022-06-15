66c2855b15adf61f632647402a55590e4ce9726c
Rikard <rikard.joohansson@gmail.com>
2022-06-15 20:30:05 +0200

    packages installed an environment fixed

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+try:
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    import requests
+    from selenium import webdriver
+        
+except ModuleNotFoundError:
+    print("Please download dependencies from requirement.txt")
+except Exception as ex:
+    print(ex)
+
 if __name__ == "__main__":
-    print("hello world")
-    print("testing")
+    # Which twitter user you would like to get data from
+    url = 'https://twitter.com/ZelenskyyUa'
+    # Send a http request to url
+    html_content=requests.get(url).text
+    soup = BeautifulSoup(html_content, "html.parser")
+    data = {}
+    tweet_data = soup.find("span", attrs={"class": "css-901oao css"})
+    print(tweet_data)
