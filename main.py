74b2a17c4fdc26b685de55a686daf8a598206f2c
Rikard <rikard.joohansson@gmail.com>
2022-06-24 10:55:42 +0200

    cleaning up in filestructure prior to starting gui

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 try:
     from scrape import storeScrape
     from scrape import scrape
+    from plot_likes_over_time import plot
     import json
     import pandas as pd
     from io import StringIO
-    import matplotlib.pyplot as plt
-    import matplotlib.dates as mdates
-    from datetime import datetime
+
 
 except ModuleNotFoundError:
     print('Please download dependencies from requirement.txt')
@@ -18,17 +17,11 @@ except Exception as ex:
 if __name__ == '__main__':
     twitter_handle = 'ZelenskyyUa';
     count = 200;
-    jsonResponse = scrape(twitter_handle, count);
-    df = storeScrape(jsonResponse)
-    df.head()
     filepath = r'C:\Users\rikar\Documents\Skola\KAU\Projekt\dataframe\df.csv'
-    df = pd.read_csv(filepath, names=('created_at','favorite_count','id', 'retweet_count','text'), header=0)    
     
-    df_created_at,df_favourite_count = pd.to_datetime(df['created_at']), df['favorite_count']
+    jsonResponse = scrape(twitter_handle, count);
+    df = storeScrape(jsonResponse, filepath)
+    plot(filepath)
+    
 
-    df.info()
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=10))
-    plt.plot(df_created_at,df_favourite_count)
-    plt.gcf().autofmt_xdate()
    
